f2299dca64ae4c2615be4d693daa9f2382874eff
Mariam Adly <mariamadly712@gmail.com>
2023-05-30 16:00:14 -0700

    add final version

--- Sport Application/Module/LeagueDetails/ViewModel/LeagueDetailsViewModel.swift
+++ Sport Application/Module/LeagueDetails/ViewModel/LeagueDetailsViewModel.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 class LeagueDetilsViewModel{
+    
+    var sportName : String?
+    var leagueId : Int?
+    var leagueDetailsViewModel : LeagueDetilsViewModel?
+    init(sportName: String? = nil, leagueId: Int? = nil, leagueDetailsViewModel: LeagueDetilsViewModel? = nil) {
+        self.sportName = sportName
+        self.leagueId = leagueId
+        self.leagueDetailsViewModel = leagueDetailsViewModel
+    }
+
    
     var bindUpComingListToLeagueDetailsVC :  (()->()) = {}
     var bindLatestEventListToLeagueDetailsVC :  (()->()) = {}
